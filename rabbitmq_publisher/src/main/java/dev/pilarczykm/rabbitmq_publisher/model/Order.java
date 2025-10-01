a76d4fd8ced410d2dbcddf0fe4b0ccea148dfb7c
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-10-01 12:40:46 +0200

    Code cleanup

--- rabbitmq_publisher/src/main/java/dev/pilarczykm/rabbitmq_publisher/model/Order.java
+++ rabbitmq_publisher/src/main/java/dev/pilarczykm/rabbitmq_publisher/model/Order.java
@@ -1,6 +1,9 @@
 package dev.pilarczykm.rabbitmq_publisher.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
