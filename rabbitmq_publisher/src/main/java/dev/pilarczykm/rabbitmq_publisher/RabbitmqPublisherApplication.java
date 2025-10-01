b4fab698c400bf3b06e2b243cba05ddd277176dc
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-10-01 11:32:18 +0200

    Project init
    rabbitmq_publisher

---
+++ rabbitmq_publisher/src/main/java/dev/pilarczykm/rabbitmq_publisher/RabbitmqPublisherApplication.java
@@ -0,0 +1,13 @@
+package dev.pilarczykm.rabbitmq_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPublisherApplication.class, args);
+	}
+
+}
