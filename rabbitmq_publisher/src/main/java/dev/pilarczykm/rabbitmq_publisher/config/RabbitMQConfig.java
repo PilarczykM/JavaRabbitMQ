8be54db08e1efdcd9fc5d163c8fae7f6351b3246
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-10-01 12:39:48 +0200

    Create rabbitmq config

---
+++ rabbitmq_publisher/src/main/java/dev/pilarczykm/rabbitmq_publisher/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package dev.pilarczykm.rabbitmq_publisher.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "order-queue";
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+        var template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
